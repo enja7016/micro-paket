6033bb769a80c1a3d5b0309ea2c320928e8e3571
enja7016 <71017303+enja7016@users.noreply.github.com>
2020-09-16 10:45:44 +0200

    Nu stannar bilderna kvar hos the receiver och ljud har tagits bort. Variablerna har även byts ut till de nya.

--- main.ts
+++ main.ts
@@ -1,70 +1,67 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    basic.clearScreen()
     if (receivedNumber == 0) {
         basic.showArrow(ArrowNames.West)
-        music.playTone(523, music.beat(BeatFraction.Whole))
     } else if (receivedNumber == 1) {
         basic.showArrow(ArrowNames.East)
-        music.playTone(523, music.beat(BeatFraction.Whole))
     } else if (receivedNumber == 2) {
         basic.showIcon(IconNames.Sad)
-        basic.pause(500)
-        music.playTone(523, music.beat(BeatFraction.Double))
     } else if (receivedNumber == 3) {
         basic.showIcon(IconNames.Happy)
-        basic.pause(500)
-        music.playTone(262, music.beat(BeatFraction.Whole))
-        music.playTone(330, music.beat(BeatFraction.Whole))
     } else if (receivedNumber == 4) {
         basic.showArrow(ArrowNames.South)
-        music.playTone(523, music.beat(BeatFraction.Whole))
     } else if (receivedNumber == 5) {
         basic.showArrow(ArrowNames.North)
-        music.playTone(523, music.beat(BeatFraction.Whole))
+    } else if (receivedNumber == 6) {
+        for (let index = 0; index < 3; index++) {
+            basic.showIcon(IconNames.Chessboard)
+            basic.showIcon(IconNames.Diamond)
+        }
     }
-    basic.clearScreen()
 })
 input.onGesture(Gesture.LogoUp, function () {
     basic.showArrow(ArrowNames.South)
-    radio.sendNumber(up)
+    radio.sendNumber(tiltUp)
 })
 input.onGesture(Gesture.TiltLeft, function () {
     basic.showArrow(ArrowNames.West)
-    radio.sendNumber(left_arrow)
+    radio.sendNumber(tiltLeft)
 })
 input.onGesture(Gesture.ScreenUp, function () {
-    radio.sendNumber(screen_up)
-    if (true) {
-        basic.showIcon(IconNames.Happy)
-        basic.pause(500)
-    }
+    basic.showIcon(IconNames.Happy)
+    radio.sendNumber(faceUp)
 })
 input.onGesture(Gesture.ScreenDown, function () {
-    radio.sendNumber(shake)
-    if (true) {
-        basic.showIcon(IconNames.Sad)
-        basic.pause(500)
+    basic.showIcon(IconNames.Sad)
+    radio.sendNumber(faceDown)
+})
+input.onGesture(Gesture.Shake, function () {
+    for (let index = 0; index < 3; index++) {
+        basic.showIcon(IconNames.Chessboard)
+        basic.showIcon(IconNames.Diamond)
+        radio.sendNumber(shake)
     }
 })
 input.onGesture(Gesture.TiltRight, function () {
     basic.showArrow(ArrowNames.East)
-    radio.sendNumber(right_arrow)
+    radio.sendNumber(tiltRight)
 })
 input.onGesture(Gesture.LogoDown, function () {
     basic.showArrow(ArrowNames.North)
-    radio.sendNumber(down)
+    radio.sendNumber(tiltDown)
 })
-let down = 0
-let up = 0
-let screen_up = 0
 let shake = 0
-let left_arrow = 0
-let right_arrow = 0
+let tiltDown = 0
+let tiltUp = 0
+let faceUp = 0
+let faceDown = 0
+let tiltLeft = 0
+let tiltRight = 0
 basic.showIcon(IconNames.Happy)
 radio.setGroup(40)
-right_arrow = 1
-left_arrow = 0
-shake = 2
-screen_up = 3
-up = 4
-down = 5
+tiltRight = 1
+tiltLeft = 0
+faceDown = 2
+faceUp = 3
+tiltUp = 4
+tiltDown = 5
+shake = 6
